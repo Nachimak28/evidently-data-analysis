515fb5b5b4868f1a4a0a9a1ecdde365a931bd774
Nachiket <nachimak28@gmail.com>
2022-07-30 19:24:09 +0530

    setup.py modifications

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     name="evidently_data_analysis",
     version="0.0.1",
     description="⚡ Lightning component ⚡ generated with command: lightning init component",
-    author="",
-    author_email="",
+    author="Nachiket Makwana",
+    author_email="nachimak28@gmail.com",
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
     url="https://github.com/Nachimak28/evidently-data-analysis",
     install_requires=[],
