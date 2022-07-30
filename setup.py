7d0adeb8655193541420d105eb6fa8af38c4ce94
Nachiket <nachimak28@gmail.com>
2022-07-30 19:23:40 +0530

    modified setup

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="evidently_data_analysis",
-    version="0.0.0",
+    version="0.0.1",
     description="⚡ Lightning component ⚡ generated with command: lightning init component",
     author="",
     author_email="",
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url="https://github.com/Lightning-AI/lightning-component-template",
+    url="https://github.com/Nachimak28/evidently-data-analysis",
     install_requires=[],
     packages=find_packages(),
 )
