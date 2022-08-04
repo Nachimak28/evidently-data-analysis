26f6073bf5cbe21da6660c1dd07b9a569bcc84cf
Nachiket <nachimak28@gmail.com>
2022-08-04 13:45:15 +0530

    changes to component for receiving payload in run

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from lightning.app.storage.payload import Payload
 import pandas as pd
 from evidently_data_analysis import EvidentlyDataAnalysis
 from lightning.app.frontend.web import StaticWebFrontend
@@ -11,22 +12,44 @@ class StaticPageViewer(L.LightningFlow):
     def configure_layout(self):
         return StaticWebFrontend(serve_dir=self.serve_dir)
 
-class LitApp(L.LightningFlow):
-    def __init__(self, train_dataframe_path, test_dataframe_path, target_column_name, task_type) -> None:
-        super().__init__()
-        self.train_dataframe_path = train_dataframe_path
-        self.test_dataframe_path = test_dataframe_path
-        self.target_column_name = target_column_name
-        self.task_type = task_type
-        self.evidently_data_analysis = EvidentlyDataAnalysis(
-                                                        train_dataframe_path=self.train_dataframe_path,
-                                                        test_dataframe_path=self.test_dataframe_path,
-                                                        target_column_name=self.target_column_name,
-                                                        task_type=self.task_type, parallel=False)
-        self.report_render = StaticPageViewer(self.evidently_data_analysis.report_parent_path)
+
+class TempWorkComponent(L.LightningWork):
+    def __init__(self, parallel=True) -> None:
+        super().__init__(parallel=parallel)
+        self.train_df = None
+        self.test_df = None
 
     def run(self):
-        self.evidently_data_analysis.run()
+        self.train_df = Payload(pd.read_csv('../ba_cancer_train.csv'))
+        self.test_df = Payload(pd.read_csv('../ba_cancer_test.csv'))
+
+class LitApp(L.LightningFlow):
+    def __init__(self, train_dataframe_path=None, test_dataframe_path=None, target_column_name=None, task_type=None) -> None:
+        super().__init__()
+        # self.train_dataframe_path = train_dataframe_path
+        # self.test_dataframe_path = test_dataframe_path
+        # self.target_column_name = target_column_name
+        # self.task_type = task_type
+        # self.evidently_data_analysis = EvidentlyDataAnalysis(
+        #                                                 train_dataframe_path=self.train_dataframe_path,
+        #                                                 test_dataframe_path=self.test_dataframe_path,
+        #                                                 target_column_name=self.target_column_name,
+        #                                                 task_type=self.task_type, parallel=False)
+        self.train_df = None
+        self.test_df = None
+
+
+
+        self.evidently_data_analysis = EvidentlyDataAnalysis(parallel=False)
+        self.report_render = StaticPageViewer(self.evidently_data_analysis.report_parent_path)
+        self.temp_component = TempWorkComponent(parallel=False)
+
+    def run(self):
+        self.temp_component.run()
+        self.evidently_data_analysis.task_type = 'classification'
+        self.evidently_data_analysis.target_column_name = 'target'
+
+        self.evidently_data_analysis.run(train_df=self.temp_component.train_df, test_df=self.temp_component.test_df)
         print(self.evidently_data_analysis.report_path)
 
     def configure_layout(self):
@@ -45,12 +68,14 @@ if __name__ == "__main__":
     train_df.to_csv('../ba_cancer_train.csv', index=False)
     test_df.to_csv('../ba_cancer_test.csv', index=False)
 
-    app = L.LightningApp(LitApp(
-            train_dataframe_path='../ba_cancer_train.csv',
-            test_dataframe_path='../ba_cancer_test.csv',
-            target_column_name='target',
-            task_type='classification'
-        ))
+    # app = L.LightningApp(LitApp(
+    #         train_dataframe_path='../ba_cancer_train.csv',
+    #         test_dataframe_path='../ba_cancer_test.csv',
+    #         target_column_name='target',
+    #         task_type='classification'
+    #     ))
+
+    app = L.LightningApp(LitApp())
 
     # regression use case
     # ca_housing_df = pd.read_csv('../ca_housing_reg.csv')
--- evidently_data_analysis/component.py
+++ evidently_data_analysis/component.py
@@ -1,5 +1,7 @@
+from cgi import test
 import os
 import lightning as L
+from lightning.app.storage.payload import Payload
 import pandas as pd
 from evidently.dashboard import Dashboard
 from evidently.pipeline.column_mapping import ColumnMapping
@@ -24,17 +26,22 @@ class EvidentlyDataAnalysis(L.LightningWork):
             raise Exception(f'task_type must be {",".join(self.supported_task_types)}')
 
 
-
-    def run(self):
+    def run(self, train_df: Payload=None, test_df: Payload=None):
         col_map = ColumnMapping()
         col_map.target = self.target_column_name
-        train_df = pd.read_csv(self.train_dataframe_path)
-        test_df = pd.read_csv(self.test_dataframe_path)
-        train_df.reset_index(drop=True, inplace=True)
-        train_df.reset_index(drop=True, inplace=True)
-        test_df.reset_index(drop=True, inplace=True)
-
+        if self.train_dataframe_path == None:
+            assert train_df is not None
+            train_df = train_df.value
+        else:
+            train_df = pd.read_csv(self.train_dataframe_path)
         
+        if self.test_dataframe_path == None:
+            assert test_df is not None
+            test_df = test_df.value
+        else:
+            test_df = pd.read_csv(self.test_dataframe_path)
+
+
         tabs = [DataDriftTab(verbose_level=0)]
         if self.task_type == 'classification':
             tabs.append(CatTargetDriftTab(verbose_level=0))
